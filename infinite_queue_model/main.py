bfc2f59d802398ca9b735a3e8bca8dfc427c413b
daudujohn <daudujoe@gmail.com>
2024-05-09 15:26:26 +0100

    feat: add system and queue wait time

--- infinite_queue_model/main.py
+++ infinite_queue_model/main.py
@@ -18,8 +18,11 @@ class FiniteQueue():
     def get_queue_length(self):
         return self.get_system_length() - self._pho
 
-    def get_system_wait_time():
-        pass
+    def get_system_wait_time(self):
+        return self.get_system_length()/self.arrival_rate
+
+    def get_queue_wait_time(self):
+        return self.get_queue_length()/self.arrival_rate
 
     def get_prob_at_least(self, n: int):
         if n == 0: return self.get_prob_zero()
@@ -45,10 +48,11 @@ if __name__ == '__main__':
                 1. System length
                 2. Queue length
                 3. System wait time
-                4. Probability of zero people in the system
-                5. Probability of N people in the system
-                6. Probability of at least N people in the system
-                7. Probability of at most N people in the system
+                4. Queue wait time
+                5. Probability of zero people in the system
+                6. Probability of N people in the system
+                7. Probability of at least N people in the system
+                8. Probability of at most N people in the system
                 """)
             try:
                 user_option = int(input("Enter choice (0 to quit): "))
@@ -60,18 +64,27 @@ if __name__ == '__main__':
             if user_option == 1:
                 val = q.get_system_length()
                 print(f"The length of the system at any given time is {format(val, '.2f')}")
+            if user_option == 2:
+                val = q.get_queue_length()
+                print(f"The length of the queue at any given time is {format(val, '.2f')}")
+            if user_option == 3:
+                val = q.get_system_length()
+                print(f"The system wait time is {format(val, '.2f')}")
             if user_option == 4:
+                val = q.get_queue_length()
+                print(f"The queue wait time is {format(val, '.2f')}")
+            if user_option == 5:
                 val = q.get_prob_zero()
                 print(f"The probability of zero people in the system is {format(val, '.2f')} or {format(val * 100, '.2f')}%")
-            if user_option == 5:
+            if user_option == 6:
                 n = int(input("Enter 'N': "))
                 val = q.get_prob_n(n)
                 print(f"The probability of {n} people in the system is {format(val, '.2f')} or {format(val * 100, '.2f')}%")
-            if user_option == 6:
+            if user_option == 7:
                 n = int(input("Enter 'N': "))
                 val = q.get_prob_at_least(n)
                 print(f"The probability of at least {n} people being in the system is {format(val, '.2f')} or {format(val * 100, '.2f')}%")
-            if user_option == 7:
+            if user_option == 8:
                 n = int(input("Enter 'N': "))
                 val = q.get_prob_at_most(n)
                 print(f"The probability of at most {n} people being in the system is {format(val, '.2f')} or {format(val * 100, '.2f')}%")
